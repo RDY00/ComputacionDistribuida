f66552773f535866ba35bc50c512c1c4b9c005e8
Fernando <fernandofong@ciencias.unam.mx>
2021-09-23 13:09:28 -0500

    Modificando el esqueleto de la práctica

--- Practica3/Consensus.ex
+++ Practica3/Consensus.ex
@@ -7,24 +7,37 @@ defmodule Consensus do
     #para que al final de un número de ejecuciones de esta,
     #todos los hilos tengan el mismo número, el cual va a ser enviado vía un
     #mensaje al hilo principal.
-    Enum.map(1..n, fn _ -> spawn(fn -> loop(:start), end) end)
+    Enum.map(1..n, fn _ ->
+      spawn(fn -> loop(:start, 0, :rand.uniform(10)), end)
+    end)
   end
 
-  defp loop(value) do
-    case value do
-      :start -> loop(value(:rand.uniform(10000)))
-      #Aquí va su código.
-    end
+  defp loop(state, value, miss_prob) do
+    if(state == :loop) do
+      loop(state, value, miss_prob)
     receive do
       {:get_value, caller} ->
 	send(caller, value)
-	loop(value)
+    after
+      1000 -> :ok
+    end
+    case value do
+      :start ->
+	chosen = :rand.uniform(10000)
+	if(rem(chosen, miss_prob) == 0) do
+	  loop(:fail, chosen, miss_prob)
+	else
+	  loop(:active, chosen, miss_prob)
+	end
+      :fail -> loop(:fail, value, miss_prob)
+      :active -> :ok #Aquí va su código.
     end
   end
 
   def consensus(processes) do
     Process.sleep(5000)
-    #Aquí va su código.
+    #Aquí va su código, deben de regresar el valor unánime decidido
+    #por todos los procesos.
     :ok
   end
   
