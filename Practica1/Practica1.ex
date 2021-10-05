ae403af4c9414d14c8d79eb938d6d2e1cad3ba5d
Fernando <fernandofong@ciencias.unam.mx>
2021-10-05 14:04:24 -0500

    Terminando práctica 1, fecha de entrega 19 de Octubre

---
+++ Practica1/Practica1.ex
@@ -0,0 +1,45 @@
+defmodule Module1 do
+
+  def fibonacci(n) do
+    :ok
+  end
+
+  def factorial(n) do
+    :ok
+  end
+
+  def random_probability(n) do
+    #Dado un número n, escoger un número aleatorio en el rango [1, n], digamos k
+    #y determinar cuál es la probabilidad de que salga un número aleatorio
+    #entre [k, n], el chiste obtener el número aleatorio.
+    :ok
+  end
+
+  def digits(n) do
+    :ok
+  end
+
+end
+
+
+defmodule Module3 do
+
+  def rev(l) do
+    :ok
+  end
+
+  def sieve_of_erathostenes(n) do
+    :ok
+  end
+  
+end
+
+defmodule Module4 do
+
+  #Implementar un autómata finito determista bajo intercambios de mensajes.
+
+  def monstructure() do
+    :ok
+  end
+  
+end
