5494702e68714b6cb60ccbe0896fde8b5b82f6a4
Esjag García <marioagd@ciencias.unam.mx>
2021-10-16 12:57:29 -0500

    Se implemento fibonacci
    La función fibonacci se implemento correctamente usando la estrucutura de control case.
    
    Adicionalemtne se hizo mínimamente robusta a fallos.
    
    Práctica: fibonacci
    Módulo: 1
    Función: fibonacci
    Autor: Mario Angel García Domínguez
    Fecha: 16 de octubre de 2021
    Para la asignatura Computación Distribuida 2022-1

--- Practica1/Practica1.ex
+++ Practica1/Practica1.ex
@@ -1,7 +1,27 @@
 defmodule Module1 do
 
+  @doc """
+    Implementación de fibonacci. Para esta función se uso case, para saber
+    cual saber que caso debe aplicar de la función recursiva. Esto por que
+    necesitamos llegar a los casos base.
+
+    (Se pudo implementar también con casa de patrones directo a las funcio-
+    nes.)
+
+    Adicionalmente checa que la entrada sea un número y que sea positivo.
+  """
   def fibonacci(n) do
-    :ok
+    case n do
+      0 -> 0
+      1 -> 1
+      n ->
+        if n > 1 && is_number(n) do
+          fibonacci(n-1) + fibonacci(n-2)
+        else
+          IO.puts("La entrada #{inspect n} es inválida.")
+          :error
+        end
+    end
   end
 
   def factorial(n) do
@@ -31,7 +51,7 @@ defmodule Module3 do
   def sieve_of_erathostenes(n) do
     :ok
   end
-  
+
 end
 
 defmodule Module4 do
@@ -39,5 +59,5 @@ defmodule Module4 do
   def monstructure() do
     :ok
   end
-  
+
 end
