85ce04d7f1b4fcefa13656e476278e13457a67c2
IanGarciaUnam <iangarcia@ciencias.unam.mx>
2021-10-15 23:48:13 -0500

    Module 1| almost finalized

--- Practica1/Practica1.ex
+++ Practica1/Practica1.ex
@@ -1,22 +1,51 @@
 defmodule Module1 do
 
   def fibonacci(n) do
-    :ok
+    case n do
+      0->0
+      1->1
+      _-> if n<0 do
+          "No es posible N<0"
+          else
+            fibonacci(n-2)+fibonacci(n-1)
+          end
+      end
   end
 
   def factorial(n) do
-    :ok
+    case n do
+      0 -> 1
+      1 -> 1
+      _ -> if n<0 do
+        "No es posible N<0"
+        else
+          n*factorial(n-1)
+        end
+      end
   end
 
   def random_probability(n) do
     #Dado un número n, escoger un número aleatorio en el rango [1, n], digamos k
     #y determinar cuál es la probabilidad de que salga un número aleatorio
     #entre [k, n], el chiste obtener el número aleatorio.
-    :ok
+
+    #numero = :rand.uniform(n)
+    #if(n-numero) == 0 do
+    #  0
+    #else
+      #1/(n-numero)
+    #end
+    numero =:rand.uniform(n)
+    if (n-numero) == 0 do
+      0
+    else
+      1/(n-numero)
+    end
   end
 
   def digits(n) do
-    :ok
+    s=to_string(n)
+    s <> <<0>>
   end
 
 end
@@ -31,7 +60,7 @@ defmodule Module3 do
   def sieve_of_erathostenes(n) do
     :ok
   end
-  
+
 end
 
 defmodule Module4 do
@@ -39,5 +68,5 @@ defmodule Module4 do
   def monstructure() do
     :ok
   end
-  
+
 end
