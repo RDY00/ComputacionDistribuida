359bcba252a430c88ba0c2ef7bd5484e5a678e0a
Fernando <fernandofong@ciencias.unam.mx>
2021-12-06 11:57:20 -0600

    Agregué pruebas de práctica 3

---
+++ Practica3/ConsensusTests.ex
@@ -0,0 +1,22 @@
+require Consensus
+
+defmodule ConsensusTests do
+
+  test "Consenso" do
+    processes = Consensus.create_consensus(10)
+    Consensus.consensus(processes) #Esto ya lo va a dormir un rato.
+    values = Enum.map(processes, fn pid ->
+      send(pid, {:get_value, caller})
+      receive do
+	x -> x
+      after
+	1000 -> :ok
+      end)
+    values = Enum.filter(values, fn x -> x != :ok end)
+    first = Enum.at(values, 0)
+    Enum.each(values, fn x ->
+      assert x == first
+    end)
+  end
+  
+end
